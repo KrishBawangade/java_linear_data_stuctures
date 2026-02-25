2cd4839ab2b47336524394febaabb2be720dc397
KrishBawangade <krishbawangade08@gmail.com>
2026-02-25 07:03:52 +0530

    Check Linked List contains a cycle or not

---
+++ linked_lists/imp_questions/linked_list_cycle/ListNode.java
@@ -0,0 +1,64 @@
+package linked_lists.imp_questions.linked_list_cycle;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode insert(ListNode head, int val){
+        ListNode temp = head;
+        ListNode node = new ListNode(val);
+
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+
+        temp.next = node;
+
+        return node;
+    }
+
+    public void display(ListNode head){
+        ListNode temp = head;
+
+        while(temp!=null){
+            System.out.printf("%d -> ", temp.val);
+            temp = temp.next;
+
+            if(temp == null){
+                System.out.print("End");
+            }
+        }
+        System.out.println();
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while(fast!=null){
+            int i=2;
+
+            // moving fast pointer ahead by 2 pos
+            while(fast!=null && i>0){
+                fast = fast.next;
+                i--;
+            }
+
+            // moving slow pointer ahead by 1 pos
+            slow = slow.next;
+
+            if(fast!= null && fast == slow){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void setNext(ListNode next){
+        this.next = next;
+    }
+}
---
+++ linked_lists/imp_questions/linked_list_cycle/Main.java
@@ -0,0 +1,16 @@
+package linked_lists.imp_questions.linked_list_cycle;
+
+public class Main {
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(1);
+
+        ListNode cycleNode = head.insert(head, 2);
+        head.insert(head, 3);
+        ListNode tail = head.insert(head, 4);
+
+        tail.setNext(cycleNode);
+
+        System.out.println(head.hasCycle(head));
+    }
+}
