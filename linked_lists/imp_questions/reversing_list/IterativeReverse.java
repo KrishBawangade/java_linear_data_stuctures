92417476deec58c5ce3824d8e39385a87b8b1130
KrishBawangade <krishbawangade08@gmail.com>
2026-02-28 11:51:37 +0530

    Reverse the linked list using iterative approach

---
+++ linked_lists/imp_questions/reversing_list/IterativeReverse.java
@@ -0,0 +1,18 @@
+package linked_lists.imp_questions.reversing_list;
+
+public class IterativeReverse {
+    public static void main(String[] args){
+        ListNode head = new ListNode(1);
+
+        head.insert(head, 2);
+        head.insert(head, 3);
+        head.insert(head, 4);
+        head.insert(head, 5);
+
+        head.display(head);
+
+        ListNode reversedHead = head.reverseList(head);
+        
+        reversedHead.display(reversedHead);
+    }
+}
---
+++ linked_lists/imp_questions/reversing_list/ListNode.java
@@ -0,0 +1,88 @@
+package linked_lists.imp_questions.reversing_list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public void setNext(ListNode next){
+        this.next = next;
+    }
+
+    public int getVal(){
+        return this.val;
+    }
+
+    public ListNode insert(ListNode head, int val){
+        ListNode temp = head;
+        ListNode node = new ListNode(val);
+
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+
+        temp.next = node;
+
+        return node;
+    }
+
+    public void display(ListNode head){
+        ListNode temp = head;
+
+        while(temp!=null){
+            System.out.printf("%d -> ", temp.val);
+            temp = temp.next;
+
+            if(temp == null){
+                System.out.print("End");
+            }
+        }
+        System.out.println();
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode first = head;
+        ListNode second = first.next;
+        ListNode temp = second.next;
+
+        while(true){
+            first.next = prev;
+            second.next = first;
+
+            if(temp == null){
+                return second;
+            }else{
+                prev = second;
+            }
+
+            if(temp.next == null){
+                temp.next = prev;
+                return temp;
+            }else{
+                first = temp;
+                temp = temp.next;
+                first.next = prev;
+            }
+
+            if(temp.next == null){
+                temp.next = first;
+                return temp;
+            }else{
+                second = temp;
+                temp = temp.next;
+                second.next = first;
+            }
+
+        }
+
+    }
+}
